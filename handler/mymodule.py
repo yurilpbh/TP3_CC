1a44093293023170ba9896ad12db601d72c64321
Yuri Pereira <yuri@anaai.com.br>
2025-01-18 16:28:06 -0300

    including timestamp

--- handler/mymodule.py
+++ handler/mymodule.py
@@ -4,6 +4,7 @@ def main(input: dict, context: object) -> dict[str, any]:
     metrics = {}
     metrics['percent-network-egress'] = round(input['virtual_memory-buffers']/input['virtual_memory-total'], 2)
     metrics['percent-memory-caching'] = round(input['virtual_memory-cached']/input['virtual_memory-total'], 2)
+    metrics['timestamp'] = input['timestamp']
     if not hasattr(context, 'monitoring_interval'):
         avg_window = 60/5
     else:
